72a83710a04e0306bb2ae10866b1783e750f4161
ASiF <www.aba333@gmail.com>
2020-07-30 04:25:23 +0600

    T2M2F3

--- controller/admin.js
+++ controller/admin.js
@@ -2,9 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
-const info = [];
-
-router.get("/", (req, res) => {
+router.get("/", function (req, res) {
   res.render("admin");
 });
+
+router.post("/", function (req, res) {
+  
+  if (req.body.addEmp == "addEmp") {
+    res.redirect("/admin/addEmployee");
+  } else if (req.body.allEmp == "allEmp") {
+    res.redirect("/admin/allEmployeeList");
+  } else {
+    res.render("admin");
+  }
+});
+
+router.get("/addEmployee", function (req, res) {
+  res.render("addEmployee");
+});
+
+router.get("/allEmployeeList", function (req, res) {
+  res.render("allEmployeeList");
+});
+
 exports.routes = router;
